23c728a3bc8b4df8b0c6710b966de085818c4218
ungaratto93 <eduardo.ungarattolima@gmail.com>
2023-02-16 14:46:22 -0300

    update project folders

---
+++ hackerrank/challenges/python/5-loops/5-python-loops.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(raw_input())
+    x = 0;
+    while x < n: 
+        print(x * x )
+        x = x + 1
--- hackerrank/domains/python/5-python-loops.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    n = int(raw_input())
-    x = 0;
-    while x < n: 
-        print(x * x )
-        x = x + 1
